a0ae46f89ff91d483ce8953e53140eeecd442d2b
jsandas <jsandas@users.noreply.github.com>
2026-01-03 08:44:40 -0700

    add watcom2 folder to gitignore

---
+++ setvars.sh
@@ -0,0 +1,7 @@
+export WATCOM=watcom2
+export PATH=$PATH:$WATCOM/bino64
+export EDDAT=$WATCOM/eddat
+
+# For DOS 8088/8086 development
+export INCLUDE=$WATCOM/h
+export LIB=$WATCOM/lib286 # You don't really need this
