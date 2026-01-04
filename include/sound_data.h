2f06e18917b798477a8bdf41450150b88502075f
jsandas <jsandas@users.noreply.github.com>
2026-01-03 21:41:15 -0700

    removed duplicate note definitions

--- include/sound_data.h
+++ include/sound_data.h
@@ -6,29 +6,10 @@
  */
 
 #ifndef SOUND_DATA_H
-#define SOUND_DATA_DATA_H
+#define SOUND_DATA_H
 
 #include <stdint.h>
-
-#define NOTE_C3         0x23a1  /* 130.817 Hz */
-#define NOTE_D3         0x1fbe  /* 146.835 Hz */
-#define NOTE_E3         0x1c48  /* 164.804 Hz */
-#define NOTE_F3         0x1ab1  /* 174.621 Hz */
-#define NOTE_G3         0x17c8  /* 195.989 Hz */
-#define NOTE_A3         0x1530  /* 219.982 Hz */
-#define NOTE_B3         0x12e0  /* 246.933 Hz */
-#define NOTE_C4         0x11d1  /* 261.605 Hz */
-#define NOTE_D4         0x0fdf  /* 293.670 Hz */
-#define NOTE_E4         0x0e24  /* 329.608 Hz */
-#define NOTE_F4         0x0d59  /* 349.190 Hz */
-#define NOTE_Fsharp4    0x0c99  /* 369.979 Hz */
-#define NOTE_G4         0x0be4  /* 391.978 Hz */
-#define NOTE_A4         0x0a98  /* 439.964 Hz */
-#define NOTE_B4         0x0974  /* 493.050 Hz */
-#define NOTE_C5         0x08e9  /* 523.096 Hz */
-#define NOTE_D5         0x07f1  /* 586.907 Hz */
-
-#define SOUND_TERMINATOR 0x0000
+#include "sound.h"
 
 /* Title sequence music - melody from the original game */
 static const uint16_t SOUND_TITLE[] = {
